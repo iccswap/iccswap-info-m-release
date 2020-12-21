ff30a63057b9966e32fdeb9f129474a9954b2312
xiaojie <xiaoj@benrongfax.com>
2020-12-21 16:03:35 +0800

    初始版本

---
+++ precache-manifest.00f8c3d232158b734e7e1c63aa45f6dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8d3b6d4d568d8e92e0b8a493f888826",
+    "url": "./index.html"
+  },
+  {
+    "revision": "052d9f55328a44dbe3ba",
+    "url": "./static/js/2.5bdfa9aa.chunk.js"
+  },
+  {
+    "revision": "5743f368e1dec8f9e2ff12b571dbc15d",
+    "url": "./static/js/2.5bdfa9aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8783eccda57f5a96acc5",
+    "url": "./static/js/main.59dd1914.chunk.js"
+  },
+  {
+    "revision": "83fad19061fafac70f7a",
+    "url": "./static/js/runtime-main.d747fa24.js"
+  },
+  {
+    "revision": "73dabb371ddce7c640334fe0904dcddf",
+    "url": "./static/media/eth.73dabb37.png"
+  }
+]);
